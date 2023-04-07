0506cbfd4733646729bda8a0538b9ec1cf0122b0
Alexandre Leroux <alex@sherpa.one>
2023-04-07 12:35:35 +0200

    useState + map

--- src/App.js
+++ src/App.js
@@ -4,10 +4,27 @@ import Bonjour from './components/Bonjour';
 import Meteo from './components/Meteo';
 
 function App() {
+
+  const people = [
+    { name: "Douaa", id: 1 },
+    { name: "Marie", id: 2 },
+    { name: "Adrien", id: 3 },
+    { name: "Adella", id: 4 },
+    { name: "Anthony", id: 5 },
+    { name: "Clara", id: 6 },
+    { name: "Marie", id: 7 },
+  ];
+
+
   return (
     <div className="App">
+
+      {people.map((item, index) => <Bonjour key={index} prenom={item.name} />)}
+
       <Hello />
+
       <Bonjour prenom="Alex" />
+
       <Meteo temps="Pluie" />
     </div>
   );
--- src/components/Meteo.js
+++ src/components/Meteo.js
@@ -1,24 +1,42 @@
+import { useState } from "react";
+
 function Meteo({ temps }) {
 
-    const imageDynamique = temps === "Soleil" ? "ciel-clair.png" : "pluvieux.png";
+    const [climat, setClimat] = useState(temps);
+    //affecte la valeur de temps à la variable d'état nommée climat
+
+    const imageDynamique = climat === "Soleil" ? "ciel-clair.png" : "pluvieux.png";
 
     function getImage(param) {
         return (param === "Soleil") ? "ciel-clair.png" : "pluvieux.png";
     }
 
     function getAlt() {
-        return temps;
+        return climat;
+    }
+
+    function onChangeMeteo() {
+        setClimat((climat === "Soleil") ? "Pluie" : "Soleil");
+        console.log(climat);
     }
 
     return (<div>
-        <img src={"images/" + imageDynamique} alt={temps} />
 
-        <img src={"images/" + getImage(temps)} alt={getAlt()} />
+        <p>
+            <button onClick={() => onChangeMeteo()}>Changer météo</button>
+        </p>
 
-        {temps === "Soleil" ? <img src="images/ciel-clair.png" alt="Soleil" /> : <img src="images/pluvieux.png" alt="Pluie" />}
+        <div>
 
-        {temps === "Soleil" && <img src="images/ciel-clair.png" alt="Soleil" />}
-        {temps === "Pluie" && <img src="images/pluvieux.png" alt="Pluie" />}
+            <img src={"images/" + imageDynamique} alt={climat} />
+
+            <img src={"images/" + getImage(climat)} alt={getAlt()} />
+
+            {climat === "Soleil" ? <img src="images/ciel-clair.png" alt="Soleil" /> : <img src="images/pluvieux.png" alt="Pluie" />}
+
+            {climat === "Soleil" && <img src="images/ciel-clair.png" alt="Soleil" />}
+            {climat === "Pluie" && <img src="images/pluvieux.png" alt="Pluie" />}
+        </div>
 
         {/* <img src="images/ciel-clair.png" alt="Soleil" />
         <img src="images/pluvieux.png" alt="Pluie" /> */}
